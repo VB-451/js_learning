f7b3f03b96a6414803a5df4348b656dbccd6baaf
Vlad Burduhosu <vlad.burdduhosu@gmail.com>
2023-11-04 14:08:29 +0200

    Task1 Result

---
+++ js_basics_arrays/Task1.js
@@ -0,0 +1,22 @@
+arr = [1,2,3,4,5];
+n = arr.length;
+
+//for:
+for (let i =0; i<n; i++){console.log(arr[i]);}
+
+//for in
+for (key in arr){console.log(arr[key]);}
+
+//for of
+for (item of arr){console.log(arr[item-1]);}
+
+//forEach
+arr.forEach(element => {
+    console.log(element);    
+});
+
+//map
+arr.map(item=>{console.log(item)})
+
+//another way
+console.log(arr);
