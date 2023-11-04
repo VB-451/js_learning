0f15b3fe28acb4ec9891f8e3fa2db9779f4c1d1d
Vlad Burduhosu <vlad.burdduhosu@gmail.com>
2023-11-04 19:26:30 +0200

    Task2 Result

---
+++ js_basics_arrays/Task2.js
@@ -0,0 +1,36 @@
+let numbers = [1, 5, 0, -5, 30, 100, -500, 0, 100, 55];
+
+const lessThanFive = (arr) => {
+    for (let item of arr){
+        if (item<5) console.log(item);
+    }
+}
+
+const minMaxIndex = (arr) =>{
+    let min=arr[0];
+    let max=arr[0];
+    for (let item of arr){
+        if (item<min) min=item;
+        if (item>max) max=item;
+    }
+    let minIndex = arr.findIndex(item =>item==min);
+    let maxIndex = arr.findIndex(item =>item==max);
+    return [minIndex,maxIndex];
+}
+
+const reverseSort = (arr) =>{
+    arr.sort((a,b)=>a-b);
+    arr.reverse();
+}
+
+const lessThanAverage = (arr) =>{
+    let average=0;
+    let sum = 0;
+    for (let item of arr){
+        sum+=item;
+    }
+    average = sum/arr.length;
+    for (let item of arr){
+        if (item<average) console.log(item);
+    }
+}
