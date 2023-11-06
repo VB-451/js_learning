74d6fc090d043df10227b2696026f28db21d1160
Vlad Burduhosu <vlad.burdduhosu@gmail.com>
2023-11-06 15:47:33 +0200

    Task3 Result

---
+++ js_basics_arrays/Task3.js
@@ -0,0 +1,10 @@
+let styles = ["Джаз","Блюз"];
+
+styles.push("Рок-н-ролл");
+
+if(styles.length%2!=0) styles.splice((styles.length/2)-0.5,1,"«Классика»")
+else styles.splice((styles.length/2),0,"«Классика»");
+
+console.log(styles.shift());
+
+styles.unshift("Рэп","Регги");
