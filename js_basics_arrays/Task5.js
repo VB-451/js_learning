3003a8fc0cd323f2d47a5cef4e932292db9a9be8
Vlad Burduhosu <vlad.burdduhosu@gmail.com>
2023-11-07 15:03:02 +0200

    Create Task5.js

---
+++ js_basics_arrays/Task5.js
@@ -0,0 +1,14 @@
+const divTwo = (num) =>{
+    return num/2;
+}
+
+const convert = (fn,arr) =>{
+    let convArr=[];
+    for (let item of arr){
+        convArr.push(fn(item));
+    }
+    return convArr;
+}
+
+let array = [2,4,6,8,10];
+console.log(convert(divTwo,array));
