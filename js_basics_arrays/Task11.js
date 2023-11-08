04eaccc36ca6bed2c25d0cab4bdfca86f5c37221
Vlad Burduhosu <vlad.burdduhosu@gmail.com>
2023-11-08 19:17:56 +0200

    Task 11 Result

---
+++ js_basics_arrays/Task11.js
@@ -0,0 +1,6 @@
+let colors = ["white", "blue", "yellow", "black", "red", "green"];
+let [firstColor, secondColor, ...otherColors] = colors;
+console.log(firstColor);
+console.log(secondColor);
+console.log(otherColors);
+
