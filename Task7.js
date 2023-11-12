9ca79062aef51f881bba31fa1cc52b511d1c1e4b
Vlad Burduhosu <114860564+VB-451@users.noreply.github.com>
2023-11-12 14:35:57 +0300

    Update Task7.js

--- Task7.js
+++ Task7.js
@@ -1,12 +1,12 @@
 let age;
 //first option:
 
-if (!((age>=14)&&(age<=90))){
+if (!((age >= 14) && (age <= 90))){
     console.log("Valid");
 }
 
 //second option:
 
-if ((age<=14)||(age>=90)){
+if ((age < 14) || (age > 90)){
     console.log("Valid");
 }
