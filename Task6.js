18e332435e177e994e94996b76936ba94b658b93
Vlad Burduhosu <114860564+VB-451@users.noreply.github.com>
2023-11-12 13:24:43 +0300

    Update Task6.js

--- Task6.js
+++ Task6.js
@@ -1,5 +1,10 @@
-let value = prompt('Enter value:','');
-if (value > 0){alert(1);}
-else if (value === 0){alert(0);}
-else if (value < 0){alert(-1);}
-else {alert('Error');}
+let value = prompt('Enter value:');
+if (value > 0){
+    alert(1);
+} else if (value === 0){
+    alert(0);
+} else if (value < 0){
+    alert(-1);
+} else {
+    alert('Error');
+}
