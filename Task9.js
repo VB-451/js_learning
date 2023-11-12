f75b9ca21d4caa9729cc218845b91e454ecc7b66
Vlad Burduhosu <114860564+VB-451@users.noreply.github.com>
2023-11-12 15:01:48 +0300

    Update Task9.js

--- Task9.js
+++ Task9.js
@@ -1,5 +1,8 @@
-let userName = prompt('Введите ваше имя:','');
+const userName = prompt('Введите ваше имя:');
 alert(`Привет, ${userName}`);
-let JSKnowledge = confirm("Знакомы ли вы с JavaScrypt?");
-if (JSKnowledge){alert("Молодец");}
-else{alert("Можно начать изучение прямо сейчас");}
+const JSKnowledge = confirm("Знакомы ли вы с JavaScrypt?");
+if (JSKnowledge){
+  alert("Молодец");
+} else {
+  alert("Можно начать изучение прямо сейчас");
+}
