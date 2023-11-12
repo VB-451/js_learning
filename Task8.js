6a24a992ee0cb68753ffe4ac75ef767001880e4a
Vlad Burduhosu <114860564+VB-451@users.noreply.github.com>
2023-11-12 14:58:08 +0300

    Update Task8.js

--- Task8.js
+++ Task8.js
@@ -1,8 +1,14 @@
-let username="";
-let password="";
-let usernameInput="";
-let passwordInput="";
-if (usernameInput===""){console.log("Ошибка Валидации");}
-else if (usernameInput!=username){console.log("Неправильный username");}
-else if ((usernameInput===username)&&(passwordInput!=password)){console.log("Неправильный пароль");}
-else if ((usernameInput===username)&&(passwordInput===password)){console.log("Привет");}
+const username = "";
+const password = "";
+const checkUser = (usernameInput, passwordInput) =>{
+  if (!usernameInput){
+    console.log("Ошибка Валидации");
+  } else if (usernameInput !== username){
+    console.log("Неправильный username");
+  } else if ((usernameInput === username) && (passwordInput !== password)){
+    console.log("Неправильный пароль");
+  } else if ((usernameInput === username) && (passwordInput === password)){
+    console.log("Привет");
+  }
+}
+
