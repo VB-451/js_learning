1dbb2f4eff2fea255f7d7f96e31d1a59efde4337
Vlad Burduhosu <114860564+VB-451@users.noreply.github.com>
2023-11-17 22:05:28 +0300

    Update Task10.js

--- js_basics_arrays/Task10.js
+++ js_basics_arrays/Task10.js
@@ -1,7 +1,6 @@
 let array = [8, 7,  2,  2,  3, 4, 2,  5, 5, 6, 7, 8, 5, 9, 0, 1];
 const uniqueSorted = (arr)=>{
     arr.sort((a, b) => a - b);
-    let sorted = [... new Set(arr)];
-    return sorted;
+    return [... new Set(arr)];
 }
 console.log(uniqueSorted(array));
