759662edc84c9721fc176d94e9410573dd8f555a
Vlad Burduhosu <vlad.burdduhosu@gmail.com>
2023-11-17 15:33:56 +0200

    Task 10 Revised

--- js_basics_arrays/Task10.js
+++ js_basics_arrays/Task10.js
@@ -1,13 +1,7 @@
 let array = [8, 7,  2,  2,  3, 4, 2,  5, 5, 6, 7, 8, 5, 9, 0, 1];
 const uniqueSorted = (arr)=>{
-    let sorted = arr.slice();
-    sorted.sort((a, b) => a - b);
-    for (let i=0; i < sorted.length - 2; i++){
-        if(sorted[i] === sorted[i+1]){
-            sorted.splice(i, 1);
-            i--;
-        }
-    }
+    arr.sort((a, b) => a - b);
+    let sorted = [... new Set(arr)];
     return sorted;
 }
 console.log(uniqueSorted(array));
