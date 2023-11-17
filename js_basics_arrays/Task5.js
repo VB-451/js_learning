0ab0c31219b1e480c72fc54815fa41b53e38da2e
Vlad Burduhosu <vlad.burdduhosu@gmail.com>
2023-11-17 17:30:43 +0200

    Task 5 Revised

--- js_basics_arrays/Task5.js
+++ js_basics_arrays/Task5.js
@@ -2,14 +2,9 @@ const divTwo = (num) =>{
     return num / 2;
 }
 
-const convert = (fn,arr) =>{
-    let convArr = [];
-    for (let item of arr){
-        convArr.push(fn(item));
-    }
-    return convArr;
+const convert = (fn, arr) =>{
+    return arr.map(fn);
 }
 
-let array = [2,4,6,8,10];
-console.log(convert(divTwo,array));
-console.log(array);
+let array = [2, 4, 6, 8, 10];
+console.log(convert(divTwo, array));
