cd397c7f126b0e87fdb73172fecaaf64a99cc973
Vlad Burduhosu <vlad.burdduhosu@gmail.com>
2023-11-25 11:25:45 +0200

    Task 10 Result

---
+++ js_basics_objects/Task10.js
@@ -0,0 +1,32 @@
+person1 = {
+    name: `Person1`,
+	age: 123,
+	salary: 1542.33,
+	contacts: {
+		phone: `112`,
+		email: 'email@domain.com'
+    },
+address: `Moldova`
+}
+person2 = {
+    name: `Person2`,
+	age: 12,
+	salary: 1542.33,
+	contacts: {
+		phone: `113`,
+		email: 'email@domain.co'
+    },
+address: `Moldova`
+}
+const intersection = (obj1, obj2)=>{
+    let resultObj={};
+    for (let key in obj1){
+        if (obj1[key] === obj2[key]){
+            resultObj[key] = obj1[key];
+        } else if (typeof obj1[key] === 'object' && Object.keys(intersection(obj1[key], obj2[key])).length !== 0){  //second part of the if condition is for it not to return an empty object
+            resultObj[key] = intersection(obj1[key], obj2[key]);
+        }
+    }
+    return resultObj;
+}
+console.log(intersection(person1, person2))
