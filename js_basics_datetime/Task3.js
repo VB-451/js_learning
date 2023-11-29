1387a6335411199b8f2c9be10ab5c7a31d838e9d
Vlad Burduhosu <vlad.burdduhosu@gmail.com>
2023-11-29 16:04:12 +0200

    Task 3 Result

---
+++ js_basics_datetime/Task3.js
@@ -0,0 +1,8 @@
+const monthNames = [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ];
+
+const month_name = (date) =>{
+    return monthNames[date.getMonth()];    
+}
+
+console.log(month_name(new Date("10/11/2009")));
+console.log(month_name(new Date("11/13/2014")));
