7390c3bd44e003ab96d7b6ff210f80b0a5633413
Vlad Burduhosu <114860564+VB-451@users.noreply.github.com>
2024-01-12 11:45:45 +0200

    Delete js_basics_datetime directory

--- js_basics_datetime/Task5.js
+++
@@ -1,4 +0,0 @@
-const timeConvert = (minutes) =>{
-  return `${minutes} minutes = ${Math.trunc(minutes / 60)} hour(s) and ${minutes % 60} minute(s)`
-}
-console.log(timeConvert(235))
--- js_basics_datetime/Task7.js
+++
@@ -1,6 +0,0 @@
-const add_years = (dateParam, years) =>{
-    dateParam.setFullYear(dateParam.getFullYear() + years);
-    return dateParam;
-}
-dt = new Date(2014,10,2);
-console.log(add_years(dt, 10).toString());
