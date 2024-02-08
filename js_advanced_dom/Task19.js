58a67f6ccf65f89a906aa5bcdea3109bd6948849
Vlad Burduhosu <114860564+VB-451@users.noreply.github.com>
2024-02-08 13:14:10 +0300

    Update Task19.js

--- js_advanced_dom/Task19.js
+++ js_advanced_dom/Task19.js
@@ -1,49 +1,44 @@
-const child = document.getElementById("child")
+const child = document.getElementById("child");
 let currentDroppable = null;
 
-child.onmousedown = function(event) {
+child.onmousedown = (event) => {
 
-let shiftX = event.clientX - child.getBoundingClientRect().left;
-let shiftY = event.clientY - child.getBoundingClientRect().top;
+    let shiftX = event.clientX - child.getBoundingClientRect().left;
+    let shiftY = event.clientY - child.getBoundingClientRect().top;
 
-child.style.position = 'absolute';
-child.style.zIndex = 1000;
-document.body.append(child);
+    child.style.position = 'absolute';
+    child.style.zIndex = 1000;
+    document.body.append(child);
 
-moveAt(event.pageX, event.pageY);
+    const moveAt = (pageX, pageY) => {
+        child.style.left = pageX - shiftX + 'px';
+        child.style.top = pageY - shiftY + 'px';
+    };
 
-function moveAt(pageX, pageY) {
-    child.style.left = pageX - shiftX + 'px';
-    child.style.top = pageY - shiftY + 'px';
-}
+    const onMouseMove = (event) => {
+        moveAt(event.pageX, event.pageY);
 
-function onMouseMove(event) {
-    moveAt(event.pageX, event.pageY);
+        child.hidden = true;
+        let elemBelow = document.elementFromPoint(event.clientX, event.clientY);
+        child.hidden = false;
 
-    child.hidden = true;
-    let elemBelow = document.elementFromPoint(event.clientX, event.clientY);
-    child.hidden = false;
+        if (!elemBelow) return;
 
-    if (!elemBelow) return;
+        let droppableBelow = elemBelow.closest('.droppable');
+        if (currentDroppable != droppableBelow) {
+            currentDroppable = droppableBelow;
+        }
+    };
 
-    let droppableBelow = elemBelow.closest('.droppable');
-    if (currentDroppable != droppableBelow) {
-    currentDroppable = droppableBelow;
-    }
-}
+    document.addEventListener('mousemove', onMouseMove);
 
-document.addEventListener('mousemove', onMouseMove);
-
-child.onmouseup = function() {
-    document.removeEventListener('mousemove', onMouseMove);
-    if (currentDroppable){
-        currentDroppable.appendChild(child);
-    }
-    child.onmouseup = null;
+    child.onmouseup = () => {
+        document.removeEventListener('mousemove', onMouseMove);
+        if (currentDroppable){
+            currentDroppable.appendChild(child);
+        }
+        child.onmouseup = null;
+    };
 };
 
-};
-
-child.ondragstart = function() {
-return false;
-};
+child.ondragstart = () => false;
